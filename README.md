1b76268e5d0f5e562f548d5513084f859e21c565
Leebecky <leebecky06@gmail.com>
2021-03-05 01:59:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cookbook_planner
+ A recipe keeper, meal planner and shopping list application
---
+++ android/app/src/main/kotlin/com/example/cookbook_planner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cookbook_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
